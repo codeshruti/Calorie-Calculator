9be7b9a61f0ed17de1bb5803762d94ba8c784ea8
S Shruti <shrutisrivatsan@gmail.com>
2022-02-25 11:53:40 +0530

    Added Recommendation System

--- multiapp.py
+++ multiapp.py
@@ -1,12 +1,14 @@
-import streamlit as st
-st.set_page_config(page_title='Calorie Calculator')
 import Calorie_calculator
 import weightloss_tracker
 import calorie_deficit
+import rec_system
+import streamlit as st
+st.set_page_config(page_title='Calorie Calculator')
 PAGES = {
     "Calorie Calculator": Calorie_calculator,
     "Calorie Deficit Calculator": calorie_deficit,
-    "Weight Loss Tracker": weightloss_tracker
+    "Weight Loss Tracker": weightloss_tracker,
+    "Recommendation System": rec_system
 }
 st.sidebar.title('Calorie cum weight loss tracker')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
